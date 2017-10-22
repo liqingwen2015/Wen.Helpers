ad6df433e4b1a412698329f85cd74178255ded80
liqingwen <liqingwen2013@outlook.com>
2017-10-22 11:39:58 +0800

    +SuperLogger

--- Wen.Helpers.ConsoleApp/Program.cs
+++ Wen.Helpers.ConsoleApp/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Wen.Helpers.Extend;
 using Wen.Helpers.Http;
+using Wen.Helpers.Log;
 
 #endregion
 
@@ -13,22 +14,12 @@ namespace Wen.Helpers.ConsoleApp
     {
         private static void Main(string[] args)
         {
-            TestApi();
-
+            SuperLogger.Register();
+            SuperLogger.Debug("Debug");
+            SuperLogger.Info("Info");
             Console.Read();
         }
 
-        private static async Task TestApi()
-        {
-            //地址
-            const string url = "http://www.ipip5.com/today/api.php?type=json";
-            //得到结果
-            var result = await HttpHelper.GetAsync(url);
-            //字符串转 json
-            var json = result.ToJson();
 
-            //想要哪个值就点出对应的属性名
-            Console.WriteLine(json.today);
-        }
     }
 }
---
+++ Wen.Helpers.Core/Log/SuperLogLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wen.Helpers.Log
+{
+    internal enum SuperLogLevel
+    {
+        Debug,
+        Info,
+        Error,
+        Warn,
+        Fatal
+    }
+}
